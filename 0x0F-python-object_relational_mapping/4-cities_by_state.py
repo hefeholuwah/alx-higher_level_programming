4cc6fc9978a03a22c19e963353bc8df88a09624e
hefeholuwah <ilieas300@gmail.com>
2022-09-12 09:36:10 -0700

    full commit

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa
-where name matches the argument
-and is safe from SQL injections"""
+"""lists all cities from the database hbtn_0e_4_usa"""
 
 if __name__ == '__main__':
 
@@ -13,8 +10,10 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (sys.argv[4],))
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
